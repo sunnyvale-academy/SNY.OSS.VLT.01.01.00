5b5f070d297cb4c859099d1baaa03a1170b7bd2d
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2023-02-22 15:29:56 +0000

    fix

--- labs/setup.sh
+++ labs/setup.sh
@@ -2,4 +2,4 @@ export VAULT_ROOT_TOKEN=$(cat ../keys.json | jq -r ".root_token")
 export VAULT_TOKEN=$VAULT_ROOT_TOKEN
 export VAULT_ADDR=http://localhost:8200
 export VAULT_UNSEAL_KEY=$(cat ../keys.json | jq -r ".unseal_keys_b64[]")
-kubectl exec vault-0 -n vault -- vault operator unseal $VAULT_UNSEAL_KEYvault secrets enable transit
+kubectl exec vault-0 -n vault -- vault operator unseal $VAULT_UNSEAL_KEY
